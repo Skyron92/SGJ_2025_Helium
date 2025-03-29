91f834434603c89cd6e007322b366dfb665ee6fb
Skyron <courtier.leo@gmail.com>
2025-03-29 13:55:20 +0100

    Water

---
+++ Assets/LeoScripts/Script/WaterDisplayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class WaterDisplayer : MonoBehaviour
+{
+    [SerializeField] private Renderer renderer;
+    private Material _waterMaterial;
+    [SerializeField, Range(0,10)] private float _waterSpeed = 1;
+    
+    private void OnEnable() {
+        _waterMaterial = renderer.material;
+        transform.DOMoveY(transform.position.y + 1f, 1f).SetEase(Ease.OutBack);
+        StartCoroutine(RevealWater());
+    }
+
+    private IEnumerator RevealWater() {
+        float dissolveLevel = 0f;
+        while (dissolveLevel < 1f) {
+            _waterMaterial.SetFloat("_DissolveLevel", dissolveLevel);
+            dissolveLevel += Time.deltaTime * _waterSpeed;
+            yield return null;
+        }
+    }
+}
