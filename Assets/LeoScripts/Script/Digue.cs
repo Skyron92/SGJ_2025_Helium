b17156f7e2ca07d6a61bd7335069ca52e461b0a9
Skyron <courtier.leo@gmail.com>
2025-03-29 22:05:51 +0100

    Continue AI

---
+++ Assets/LeoScripts/Script/Digue.cs
@@ -0,0 +1,12 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Digue : Case
+{
+    public Digue(int x, int y) : base(x, y)
+    {
+    }
+
+    public override void ApplyEffect() {
+    }
+}
--- Assets/LeoScripts/Script/Player.cs
+++ Assets/LeoScripts/Script/Player.cs
@@ -5,7 +5,12 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private int waterCount;
-   
+    public bool is_paused;
+
+    public void AddWater() {
+        waterCount += 3;
+        waterCount = Mathf.Clamp(waterCount, 0, 5);
+    }
 
     public void OnClick(InputAction.CallbackContext context) {
         if(waterCount <= 0) return;
