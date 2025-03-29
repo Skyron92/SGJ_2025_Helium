9b31011bf2cf4be08d0c6560a27b0a0d73515b8c
SoulierTom <tom.soulier@brassart.fr>
2025-03-29 19:48:36 +0100

    Menu pause (not working)

--- Assets/LeoScripts/Script/Player.cs
+++ Assets/LeoScripts/Script/Player.cs
@@ -5,16 +5,23 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private int waterCount;
+    public bool is_paused = false;
 
-    public void OnClick(InputAction.CallbackContext context) {
-        if(waterCount <= 0) return;
-        if (context.canceled) {
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit)) {
-                if (hit.transform.gameObject.TryGetComponent(out Case hitCase)) {
-                    waterCount--;
-                    hitCase.ApplyEffect();
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        
+            if (waterCount <= 0) return;
+            if (context.canceled)
+            {
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+                {
+                    if (hit.transform.gameObject.TryGetComponent(out Case hitCase))
+                    {
+                        waterCount--;
+                        hitCase.ApplyEffect();
+                    }
                 }
             }
-        }
+        
     }
 }
