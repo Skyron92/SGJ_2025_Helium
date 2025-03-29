6c676253cd3dca1a1dfdbf81e8744df1971c2850
Skyron <courtier.leo@gmail.com>
2025-03-29 16:53:53 +0100

    Jauge goutte

--- Assets/JaugeGoutte.cs
+++ Assets/JaugeGoutte.cs
@@ -1,23 +1,36 @@
+using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class JaugeGoutte : MonoBehaviour
 {
     public Image image;
-    public float eau;
-    public float reserve;
-
-    public void SetImage(float maxvalue, float value)
-    {
-        image.fillAmount = value / maxvalue;
-        Debug.Log(image.fillAmount);
+    public bool shouldDestroy;
+    private Transform _canvasTransform;
+    
+    private void OnEnable() {
+        _canvasTransform = transform.parent.parent;
+        _canvasTransform.localScale = Vector3.zero;
+        _canvasTransform.DOScale(Vector3.one, 0.35f).onComplete += () => ScaleAnimation();
     }
 
-    void Update()
-    {
-        if (image != null)
-        {
-            SetImage(reserve, eau);
+    private void ScaleAnimation() {
+        _canvasTransform.DOScale(Vector3.one * 0.9f, 0.8f).onComplete += () => 
+            _canvasTransform.DOScale(Vector3.one, .8f).onComplete += () => ScaleAnimation();
+    }
+
+    public void SetImage(float maxvalue, float value) { 
+        StartCoroutine(FillSmoothly(value / maxvalue));
+    }
+
+    private IEnumerator FillSmoothly(float targetedValue) {
+        var currentValue = image.fillAmount;
+        while (currentValue < targetedValue) {
+            currentValue += Time.deltaTime;
+            image.fillAmount = Mathf.Clamp(currentValue, 0, targetedValue);
+            yield return null;
         }
+        if(shouldDestroy) Destroy(transform.parent.parent.gameObject);
     }
 }
