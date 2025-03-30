c0bb84a5ab68b5fbf18267c3e93d07520ea14865
Skyron <courtier.leo@gmail.com>
2025-03-30 10:14:42 +0200

    Tuto block

--- Assets/ClaireScripts/TutoScript.cs
+++ Assets/ClaireScripts/TutoScript.cs
@@ -5,49 +5,43 @@ public class TutoScript : MonoBehaviour
     public GameObject[] widgets; // Assignez vos 4 widgets dans l'inspecteur
     private int currentWidgetIndex = 0;
     private bool isActive = true;
+    [SerializeField] private Player player;
 
-    void Start()
-    {
+    void Start() {
+        player.is_paused = true;
         ShowWidget(currentWidgetIndex);
     }
 
-    void Update()
-    {
+    void Update() {
         if (isActive && Input.GetMouseButtonDown(0)) // Si clic gauche
         {
             NextWidget();
         }
     }
 
-    void ShowWidget(int index)
-    {
-        // Désactiver tous les widgets
-        foreach (GameObject widget in widgets)
-        {
+    void ShowWidget(int index) {
+        // Dï¿½sactiver tous les widgets
+        foreach (GameObject widget in widgets) {
             widget.SetActive(false);
         }
 
         // Activer le widget actuel si l'index est valide
-        if (index < widgets.Length)
-        {
+        if (index < widgets.Length) {
             widgets[index].SetActive(true);
         }
     }
 
-    void NextWidget()
-    {
+    void NextWidget() {
         currentWidgetIndex++;
-        if (currentWidgetIndex < widgets.Length)
-        {
+        if (currentWidgetIndex < widgets.Length) {
             ShowWidget(currentWidgetIndex);
         }
-        else
-        {
-            foreach (GameObject widget in widgets)
-            {
+        else {
+            foreach (GameObject widget in widgets) {
                 widget.SetActive(false);
             }
-            isActive = false; // Désactiver le script
+            player.is_paused = false;
+            Destroy(gameObject); // Dï¿½sactiver le script
         }
     }
 }
